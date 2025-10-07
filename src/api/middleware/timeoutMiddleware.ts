7258b876e3c1ba5f4179e3b99d14b348426ea307
Amélie Loulergue <loulergueamelie@yahoo.fr>
2025-10-07 11:09:46 +0200

    console.log to logger.info

--- src/api/middleware/timeoutMiddleware.ts
+++ src/api/middleware/timeoutMiddleware.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import { logger } from '../utils/logs/winston';
 
 const parsePayloadSize = (size: string): number => {
     const units = { b: 1, kb: 1024, mb: 1024 * 1024, gb: 1024 * 1024 * 1024 };
@@ -15,9 +16,9 @@ export const timeoutMiddleware = (_req: Request, res: Response, next: NextFuncti
     let timeoutHandler: any;
 
     const handleTimeout = () => {
-        console.log('Timeout occurred');
+        logger.info('Timeout occurred');
         if (res.getHeader('x-processing-image') === 'true') {
-            console.log('More processing time required, resetting timeout...(big size,...?)');
+            logger.info('More processing time required, resetting timeout...(big size,...?)');
             resetTimeout();
         } else if (!res.headersSent) {
             res.status(408).send('Request timeout').end();
