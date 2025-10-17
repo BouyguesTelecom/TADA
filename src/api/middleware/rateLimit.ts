17d0603808f316b70b89d4660e0db10d6425531a
Amélie Loulergue <loulergueamelie@yahoo.fr>
2025-10-17 13:57:28 +0200

    remove header rateLimit

--- src/api/middleware/rateLimit.ts
+++ src/api/middleware/rateLimit.ts
@@ -10,9 +10,6 @@ export const rateLimitMiddleware = setRateLimit({
         return `${req.method}-${req.originalUrl}`;
     },
     handler: (req, res, next) => {
-        if (req.headers.useragent === 'gitlab-pipeline-artifactory') {
-            return next();
-        }
         return res.status(429).json({
             error: 'Too many requests, please try again later.'
         });
